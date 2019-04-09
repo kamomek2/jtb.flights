031a646a0740899cb3af7d22f011ec79d95db7f4
muerwre <gotham48@gmail.com>
2019-04-09 10:16:57 +0700

    removed comment

--- src/redux/flight/actions.ts
+++ src/redux/flight/actions.ts
@@ -1,7 +1,6 @@
 import { FLIGHT_ACTIONS } from "./constants";
 import { IFlightState } from "./reducer";
 
-// export const someAction = () => ({ type: USER_ACTIONS.SOME_ACTION });
 export const flightSetLoading = (is_loading: IFlightState['is_loading']) => ({
   type: FLIGHT_ACTIONS.SET_LOADING, is_loading,
 });
